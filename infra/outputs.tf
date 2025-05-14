9394d97efeec07cc68e210fd24ef1fce7d87cf81
Mark Jack <mark.jack@gmail.com>
2025-05-14 23:44:18 +0100

    Move infra folder to repo root

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,5 @@
+// infra/outputs.tf
+
 output "resource_group" {
   description = "Resource Group name"
   value       = azurerm_resource_group.rg.name
@@ -11,16 +13,4 @@ output "acr_login_server" {
 output "aks_fqdn" {
   description = "AKS API server FQDN"
   value       = azurerm_kubernetes_cluster.aks.fqdn
-}
-
-output "kube_config" {
-  description = "Raw kubeconfig for AKS user authentication"
-  value       = azurerm_kubernetes_cluster.aks.kube_config[0].raw_config
-  sensitive   = true
-}
-
-output "kube_admin_config" {
-  description = "Raw kubeconfig for AKS admin authentication"
-  value       = azurerm_kubernetes_cluster.aks.kube_admin_config[0].raw_config
-  sensitive   = true
 }
